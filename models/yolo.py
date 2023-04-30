be16cdbc2470c305575a83266d93abf54f8cb28d
HaHa <h.wu6@lboro.ac.uk>
2023-04-30 02:27:53 +0100

    增加CBAM模块

--- models/yolo.py
+++ models/yolo.py
@@ -315,7 +315,7 @@ def parse_model(d, ch):  # model_dict, input_channels(3)
         if m in [Conv, GhostConv, Bottleneck, GhostBottleneck, SPP, SPPF, MixConv2d, Focus, CrossConv, BottleneckCSP,
                  C3, C3TR, Shuffle_Block, conv_bn_relu_maxpool, DWConvblock, MBConvBlock, LC3,
                  RepVGGBlock, SEBlock, mobilev3_bneck, Hswish, SELayer, stem, CBH, LC_Block, Dense,
-                GhostConv, ES_Bottleneck, ES_SEModule]:
+                GhostConv, ES_Bottleneck, ES_SEModule, Conv_CBAM]:
             c1, c2 = ch[f], args[0]
             if c2 != no:  # if not output
                 c2 = make_divisible(c2 * gw, 8)
